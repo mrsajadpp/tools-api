b736064eac7c031212d641683248604163ba2b60
mrsajadpp <mrsajadpp@gmail.com>
2024-11-01 20:18:05 +0530

    bug fixed

--- api/index.ts
+++ api/index.ts
@@ -2,14 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bp = require('body-parser');
+const cors = require("cors");
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY); // Replace with your actual API key
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "*", // Allow any origin
+    methods: ["GET", "POST"], // Allow specific HTTP methods
+    allowedHeaders: ["Content-Type"] // Allow specific headers
+}));
 
 app.get("/", (req, res) => res.send("Hello world"));
 
